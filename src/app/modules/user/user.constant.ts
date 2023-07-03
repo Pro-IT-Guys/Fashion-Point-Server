22036a84f9d836bfbe69436e0a18cf026f4ce07f
AyonJD <ayonjodder177@gmail.com>
2023-07-03 11:13:27 +0600

    update user

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -6,5 +6,5 @@ export enum USER_ROLE_ENUM {
 
 export const USER_ROLE_ARRAY: string[] = Object.values(USER_ROLE_ENUM)
 
-export const USER_SEARCH_FIELDS = ['name', 'email', 'phone', 'address']
+export const USER_SEARCH_FIELDS = ['name', 'email', 'phone']
 export const USER_FILTER_FIELDS = ['role', 'isVerified']
