4bbf0229884c9af0c859d2a5093d943ff626af89
AyonJD <ayonjodder177@gmail.com>
2023-07-03 13:48:55 +0600

    get all country

--- src/app/modules/deliveryFee/deliveryFee.route.ts
+++ src/app/modules/deliveryFee/deliveryFee.route.ts
@@ -4,6 +4,7 @@ import { DeliveryFeeController } from './deliveryFee.controller'
 const router = express.Router()
 
 router.get('/', DeliveryFeeController.getFeeOfLocation)
+router.get('/all', DeliveryFeeController.getAllFees)
 router.post('/', DeliveryFeeController.createFee)
 router.patch('/:id', DeliveryFeeController.updateFee)
 
