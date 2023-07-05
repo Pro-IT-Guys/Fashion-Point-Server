08ab74d0ebf65eada9f8a21bad245aad81d8b13e
AyonJD <ayonjodder177@gmail.com>
2023-07-05 12:08:47 +0600

    order route

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -7,6 +7,7 @@ router.post('/', OrderController.createOrder)
 router.get('/', OrderController.getAllOrder)
 router.patch('/:id', OrderController.updateOrder)
 router.get('/user/:id', OrderController.getOrderByUserId)
+router.get('/order/:id', OrderController.getOrderByOrderId)
 router.get('/:id', OrderController.getOrderByOrderId)
 
 export const OrderRoute = router
